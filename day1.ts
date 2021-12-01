14b16fc44572904274d838498021065e9a074c73
kallelehtonen <coadiespla@gmail.com>
2021-12-01 21:26:49 +0200

    Added day1 b

--- day1.ts
+++ day1.ts
@@ -2,12 +2,33 @@ import * as fs from "fs";
 
 const input = fs.readFileSync("./input/day1.txt", "utf-8");
 const inputArray: string[] = input.split("\n");
-let count = 0;
 
-inputArray.forEach((line: string, index: number) => {
-  if (+line > +inputArray[index - 1]) {
-    count++;
-  }
-});
+function day1a() {
+  let count = 0;
 
-console.log(count);
+  inputArray.forEach((line: string, index: number) => {
+    if (+line > +inputArray[index - 1]) {
+      count++;
+    }
+  });
+
+  console.log(count);
+}
+
+function day1b() {
+  let count = 0;
+  const sums: number[] = inputArray.map((line: string, index: number) => {
+    return +line + +inputArray[index - 1] + +inputArray[index - 2];
+  });
+
+  sums.forEach((sum: number, index: number) => {
+    if (sum > sums[index - 1]) {
+      count++;
+    }
+  });
+
+  console.log(count);
+}
+
+day1a();
+day1b();
