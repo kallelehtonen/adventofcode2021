d0aa187a0be7206dee1cf572f692be095741e033
kallelehtonen <coadiespla@gmail.com>
2021-12-04 14:14:17 +0200

    Refactored a little

--- day4.ts
+++ day4.ts
@@ -2,11 +2,12 @@ import * as fs from "fs";
 
 const input = fs.readFileSync("./input/day4.txt", "utf-8").trim();
 const [ numbers, ...boards ] = input.split("\n\r");
-const numberArray: string[] = numbers.trim().split(",");
+const numberArray: number[] = numbers.trim().split(",").map(Number);
 
 function day4a(): number {
+  const numbers: number[] = [...numberArray];
   let winnerUnmarkedSum = 0;
-  let lastDrawnNumber = "";
+  let lastDrawnNumber = 0;
   const rowCounts = new Map<string, number>();
   const colCounts = new Map<string, number>();
   const boardSums = new Map<number, number>();
@@ -14,45 +15,45 @@ function day4a(): number {
   let initRun = true;
 
   gameLoop:
-  while (!gameWon) {
-    lastDrawnNumber = drawNumber();
+    while (!gameWon) {
+      lastDrawnNumber = drawNumber(numbers);
 
-    for (let boardIndex = 0; boardIndex < boards.length; boardIndex++) {
-      const rows: string[] = boards[boardIndex].trim().split("\n");
+      for (let boardIndex = 0; boardIndex < boards.length; boardIndex++) {
+        const rows: string[] = boards[boardIndex].trim().split("\n");
 
-      for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-        const rowNumbers: string[] = rows[rowIndex].trim().split(" ");
+        for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+          const rowNumbers: number[] = rows[rowIndex].trim().split(/\s+/).map(Number);
 
-        if (initRun) {
-          boardSums.set(boardIndex, (boardSums.get(boardIndex) || 0) + rowNumbers.map(Number).reduce((accumulator, curr) => accumulator + curr));
-        }
+          if (initRun) {
+            boardSums.set(boardIndex, (boardSums.get(boardIndex) || 0) + rowNumbers.map(Number).reduce((accumulator, curr) => accumulator + curr));
+          }
 
-        for (let colIndex = 0; colIndex < rowNumbers.length; colIndex++) {
-          if (lastDrawnNumber === rowNumbers[colIndex]) {
-            updateCount(rowCounts, boardIndex, rowIndex);
-            updateCount(colCounts, boardIndex, colIndex);
+          for (let colIndex = 0; colIndex < rowNumbers.length; colIndex++) {
+            if (lastDrawnNumber === rowNumbers[colIndex]) {
+              updateCount(rowCounts, boardIndex, rowIndex);
+              updateCount(colCounts, boardIndex, colIndex);
 
-            boardSums.set(boardIndex, (boardSums.get(boardIndex) || 0) - +lastDrawnNumber);
+              boardSums.set(boardIndex, (boardSums.get(boardIndex) || 0) - +lastDrawnNumber);
 
-            if (checkStatus(rowCounts, boardIndex, rowIndex) || checkStatus(colCounts, boardIndex, colIndex)) {
-              winnerUnmarkedSum = boardSums.get(+boardIndex) || 0;
-              gameWon = true;
-              break gameLoop;
+              if (checkStatus(rowCounts, boardIndex, rowIndex) || checkStatus(colCounts, boardIndex, colIndex)) {
+                winnerUnmarkedSum = boardSums.get(+boardIndex) || 0;
+                gameWon = true;
+                break gameLoop;
+              }
             }
           }
         }
       }
-    }
 
-    initRun = false;
-  }
+      initRun = false;
+    }
 
   return winnerUnmarkedSum * +lastDrawnNumber;
 }
 
-function drawNumber(): string {
+function drawNumber(numbers: number[]): number {
   // @ts-ignore
-  return numberArray.shift();
+  return numbers.shift();
 }
 
 function updateCount(map: Map<string, number>, board: number, index: number): void {
