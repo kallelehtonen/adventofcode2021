844f7a34fc9ac42ab86f5c4b3b204e2589e283f9
Kalle Lehtonen <kalle.lehtonen@digia.com>
2021-12-07 18:17:52 +0200

    Day 7 b

--- day7.ts
+++ day7.ts
@@ -12,8 +12,27 @@ function day7a(): number {
     .reduce((prev: number, grab: number) => prev + Math.abs(grab - target), 0);
 }
 
+function day7b(): number {
+  const originalPositions = [...positions];
+  const target: number = average(originalPositions);
+
+  return originalPositions
+    .filter((grab: number) => grab !== target)
+    .reduce((prev: number, grab: number) => prev + consumption(grab, target), 0);
+}
+
 const median = (input: number[]): number => {
   return input.slice().sort((a: number, b: number) => a - b)[Math.floor(input.length / 2)];
 }
 
+const average = (input: number[]): number => {
+  return Math.floor(input.reduce((sum: number, next: number) => sum + next, 0) / input.length);
+}
+
+const consumption = (input: number, target: number): number => {
+  const distance = Math.abs(input - target);
+  return (distance * (distance + 1)) / 2;
+}
+
 console.log("a: " + day7a());
+console.log("b: " + day7b());
