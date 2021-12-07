761ff1ceaebe924c0d2a7e980a533761591ddbaf
kallelehtonen <coadiespla@gmail.com>
2021-12-07 21:31:14 +0200

    Refactored

--- day5.ts
+++ day5.ts
@@ -1,32 +1,66 @@
 import * as fs from "fs";
 
-const input = fs.readFileSync("./input/example.txt", "utf-8").trim();
+const input = fs.readFileSync("./input/day5.txt", "utf-8").trim();
 const lines = input.split("\n");
 
+enum Direction {
+  horizontal,
+  vertical,
+  diagonal,
+}
+
+class VentLine {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+  direction: Direction = Direction.diagonal;
+  length: number;
+
+  constructor(input: string) {
+    const [a, b] = input.trim().split(" -> ");
+    [this.x1, this.y1] = a.trim().split(",").map(Number);
+    [this.x2, this.y2] = b.trim().split(",").map(Number);
+
+    if (this.x1 === this.x2) {
+      this.direction = Direction.vertical;
+    }
+
+    if (this.y1 === this.y2) {
+      this.direction = Direction.horizontal;
+    }
+
+    switch (this.direction) {
+      case Direction.vertical:
+        this.length = Math.abs(this.y1 - this.y2);
+        break;
+
+      case Direction.horizontal:
+        this.length = Math.abs(this.x1 - this.x2);
+        break;
+
+      case Direction.diagonal:
+      default:
+        this.length = 0;
+        break;
+    }
+  }
+}
+
 function day5a(): number {
-  const ventLines: string[] = [...lines];
+  const ventLines: VentLine[] = lines.map((line: string) => new VentLine(line));
   const coordsCount: Map<string, number> = new Map<string, number>();
 
   for (const ventLine of ventLines) {
-    const [a, b] = ventLine.trim().split(" -> ");
-    const [x1, y1] = a.trim().split(",").map(Number);
-    const [x2, y2] = b.trim().split(",").map(Number);
-
-    // vertical
-    if (x1 === x2) {
-      const length = Math.abs(y1 - y2);
-
-      for (let i = 0; i <= length; i++) {
-        addCount(coordsCount, x1, Math.min(y1, y2) + i);
+    if (ventLine.direction === Direction.vertical) {
+      for (let i = 0; i <= ventLine.length; i++) {
+        addCount(coordsCount, ventLine.x1, Math.min(ventLine.y1, ventLine.y2) + i);
       }
     }
 
-    // horizontal
-    if (y1 === y2) {
-      const length = Math.abs(x1 - x2);
-
-      for (let i = 0; i <= length; i++) {
-        addCount(coordsCount, Math.min(x1, x2) + i, y1);
+    if (ventLine.direction === Direction.horizontal) {
+      for (let i = 0; i <= ventLine.length; i++) {
+        addCount(coordsCount, Math.min(ventLine.x1, ventLine.x2) + i, ventLine.y1);
       }
     }
   }
