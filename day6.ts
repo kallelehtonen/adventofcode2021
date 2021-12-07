ba76b8eebe7e986b923b75750e7401388940399d
kallelehtonen <coadiespla@gmail.com>
2021-12-07 22:37:12 +0200

    Day 6 boilerplate

---
+++ day6.ts
@@ -0,0 +1,14 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync("./input/day6.txt", "utf-8").trim();
+
+function day6a(): number {
+
+}
+
+function day6b(): number {
+
+}
+
+console.log("a: " + day6a());
+console.log("b: " + day6b());
